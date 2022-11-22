3de05a620a0af2c0147d495167f69e14304eb1e4
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-22 22:35:05 +0700

    updated contract deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  paths: {
+    artifacts: './artifacts'
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  },
   solidity: "0.8.17",
 };
 
